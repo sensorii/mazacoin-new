64d6626c475bfa51e9ea9eb3e9317869d1b8927b
Xendarboh Sensorii <xen@shastafareye.net>
2015-01-18 19:31:47 -0800

    convert strings for mazacoin-cli help output

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -67,12 +67,12 @@ static bool AppInitRPC(int argc, char* argv[])
     //
     ParseParameters(argc, argv);
     if (argc<2 || mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("Bitcoin Core RPC client version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("Mazacoin Core RPC client version") + " " + FormatFullVersion() + "\n";
         if (!mapArgs.count("-version")) {
             strUsage += "\n" + _("Usage:") + "\n" +
-                  "  bitcoin-cli [options] <command> [params]  " + _("Send command to Bitcoin Core") + "\n" +
-                  "  bitcoin-cli [options] help                " + _("List commands") + "\n" +
-                  "  bitcoin-cli [options] help <command>      " + _("Get help for a command") + "\n";
+                  "  mazacoin-cli [options] <command> [params]  " + _("Send command to Mazacoin Core") + "\n" +
+                  "  mazacoin-cli [options] help                " + _("List commands") + "\n" +
+                  "  mazacoin-cli [options] help <command>      " + _("Get help for a command") + "\n";
 
             strUsage += "\n" + HelpMessageCli();
         }
